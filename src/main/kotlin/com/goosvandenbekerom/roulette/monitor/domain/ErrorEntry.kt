405397afbd8ec03723195d92a74db693c3a15b92
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-25 00:57:00 +0200

    Added default values for jpa

--- src/main/kotlin/com/goosvandenbekerom/roulette/monitor/domain/ErrorEntry.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/monitor/domain/ErrorEntry.kt
@@ -6,4 +6,4 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-data class ErrorEntry(val message: String, val context: String, val username: String, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
+data class ErrorEntry(val message: String = "", val context: String = "", val username: String = "", @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
