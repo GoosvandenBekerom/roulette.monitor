9d47027b5d0e88c9f2be1f79a3366bdb16b603fe
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-24 19:05:45 +0200

    Created error entry domain model

---
+++ src/main/kotlin/com/goosvandenbekerom/roulette/monitor/domain/ErrorEntry.kt
@@ -0,0 +1,9 @@
+package com.goosvandenbekerom.roulette.monitor.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class ErrorEntry(val message: String, val context: String, val username: String, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
